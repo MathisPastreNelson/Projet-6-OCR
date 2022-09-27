6f626256e8e4ad8c685a04eb34bdb8f831aed81c
Mathis Pastre <shiray@hotmail.fr>
2022-09-27 03:36:54 +0200

    Installation de node+express

--- back/server.js
+++ back/server.js
@@ -1,1 +1,7 @@
-"voici un test de mon git"
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la réponse du serveur !');
+});
+
+server.listen(process.env.PORT || 4200);
