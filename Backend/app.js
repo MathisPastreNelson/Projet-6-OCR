a0ad0e1c7885bcd53d18da7ecf90560898c38152
MathisPastreNelson <shiray@hotmail.fr>
2022-10-02 18:45:59 +0200

    Enregistrement de l'utilisateur dans la DB ok

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 
 
 const userRoutes = require("./routes/users")
-const userCtrl = require('./controllers/users');
 
 const router = express.Router();
 const app = express();
@@ -28,22 +27,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     // Permet d'envoyer des requêtes avec les méthodes mentionnées
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
     next();
 });
 
