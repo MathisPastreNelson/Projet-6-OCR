8cf51e15289df8c02646cd3ad2634b542a193f95
MathisPastreNelson <shiray@hotmail.fr>
2022-10-04 21:52:30 +0200

    Ajout commentaire

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
     try {
         // Récupère l'authorisation après l'espace dans le HEADER
         const token = req.headers.authorization.split(' ')[1];
+        // Permet de vérifier la validité d'un token
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = {
