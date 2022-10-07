8252c9b26baa25d06fbc07368adb1f360dd81f3b
MathisPastreNelson <shiray@hotmail.fr>
2022-10-07 20:23:41 +0200

    Debbug de Multer OK ajout sur les routes put post

--- Backend/middleware/multer-config.js
+++ Backend/middleware/multer-config.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 
-// Dictionnaire :
+// Dictionnaire des MIME_TYPES :
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
@@ -18,11 +18,11 @@ const storage = multer.diskStorage({
         const name = file.originalname.split(' ').join('_');
         // L'extension du fichier sera traduit en élément du dictionnaire
         const extension = MIME_TYPES[file.mimetype];
-        // Création du filename + timestamp + . + extension du fichier
-        callback(null, name + Date.now() + '.' + extension);
+        // Création du filename + timestamp + extension du fichier
+        callback(null, name + '_' + Date.now() + extension);
     }
 });
 // console.log(extension)
 
 // Exportation du middleware MULTER, on lui passe l'objet stocké, la méthode single() car c'est un fichier unique et on précise que c'est une image
-module.exports = multer({ storage: storage }).single('image');
+module.exports = multer({ storage }).single('image');
