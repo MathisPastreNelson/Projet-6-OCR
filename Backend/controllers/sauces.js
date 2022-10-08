85139dab332c5b6c1560440fe0715673e2466472
MathisPastreNelson <shiray@hotmail.fr>
2022-10-08 19:57:10 +0200

    Fonction Get product pas encore validé

--- Backend/controllers/sauces.js
+++ Backend/controllers/sauces.js
@@ -8,40 +8,35 @@ exports.createSauce = (req, res, next) => {
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        // _id: sauceObject._id
     });
     // Je sauvegarde la nouvelle sauce dans DB
-    sauce.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Sauce correctement ajoutée !'
-            });
-        }
-    ).catch(
-        (error) => {
+    sauce.save().then(() => {
+        res.status(201).json({
+            message: 'Sauce correctement ajoutée !'
+        });
+    })
+        .catch((error) => {
             res.status(400).json({
                 error: error
             });
-        }
-    );
+        });
 };
 
 exports.getOneSauce = (req, res, next) => {
     // Recherche d'une sauce par son ID
-    Sauce.findOne({
-        _id: req.params.id
-    }).then(
-        (Sauces) => {
-            res.status(200).json(Sauces);
-            console.log(Sauces)
-        }
-    ).catch(
-        (error) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then((thing) => {
+            res.status(200).json(thing);
+            console.log("PARAMSID : ", req.params.id)
+            console.log("ObjetComplet : ", thing)
+        })
+        .catch((error) => {
             res.status(404).json({
                 error: error
             });
-        }
-    );
+        });
 };
 
 exports.modifySauce = (req, res, next) => {
@@ -53,35 +48,30 @@ exports.modifySauce = (req, res, next) => {
         price: req.body.price,
         userId: req.body.userId
     });
-    sauce.updateOne({ _id: req.params.id }, sauce).then(
-        () => {
-            res.status(201).json({
-                message: 'Sauce mise à jour !!'
-            });
-        }
-    ).catch(
-        (error) => {
+    sauce.updateOne({ _id: req.params.id }, sauce).then(() => {
+        res.status(201).json({
+            message: 'Sauce mise à jour !!'
+        });
+    })
+        .catch((error) => {
             res.status(400).json({
                 error: error
             });
-        }
-    );
+        });
 };
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Sauce supprimée !'
+    Sauce.deleteOne({ _id: req.params.id }).then(() => {
+        res.status(200).json({
+            message: 'Sauce supprimée !'
+        });
+    })
+        .catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
             });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
 };
 
 exports.getAllSauce = (req, res, next) => {
@@ -89,12 +79,11 @@ exports.getAllSauce = (req, res, next) => {
     Sauce.find().then(
         (Sauces) => {
             res.status(200).json(Sauces);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
+        })
+        .catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
             });
-        }
-    );
 };
