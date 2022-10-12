844729cd07003f724a0cb50ca092874fc4bbc253
MathisPastreNelson <shiray@hotmail.fr>
2022-10-12 16:47:33 +0200

    L'utilisateur peut liker une sauce (pas encore dislike ni retiré son like)

--- Backend/controllers/sauces.js
+++ Backend/controllers/sauces.js
@@ -57,7 +57,7 @@ exports.modifySauce = (req, res, next) => {
                 const filename = sauce.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, () => {
                     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-                        .then(() => res.status(200).json({ message: 'Sauce modifiée!' }))
+                        .then(() => res.status(200).json({ message: 'Sauce modifiée, nouvelle photo active !' }))
                         .catch(error => res.status(401).json({ error }));
                 })
                 // Si l'image n'a pas été modifié on change uniquement les données
@@ -111,15 +111,12 @@ exports.getAllSauce = (req, res, next) => {
 // Essai de like/dislike
 exports.likeSauce = (req, res, next) => {
     console.log("Je suis dans le controller like")
-
     // Affichage du req.body
     console.log("--->Contenu req.body --Ctrl Like")
     console.log(req.body)
-
     // Récupéré l'id dans l'url de la requête
     console.log("--->Contenu req.params --Ctrl Like")
     console.log(req.params)
-
     // Mise au format de l'id pour pouvoir aller chercher l'objet correspondant dans la base de données
     console.log("--->id en _id --Ctrl Like")
     console.log({ _id: req.params.id })
@@ -127,16 +124,34 @@ exports.likeSauce = (req, res, next) => {
     // Aller chercher l'objet dans la DB
     Sauce.findOne()
         .then((sauce) => {
-            res.status(200).json(sauce)
+            console.log("--> Contenu resultat promise : Objet --Ctrl Like", sauce);
+            // Like = 1 (likes = +1)
+            // Utilisation de la méthode javascript includes()
+            // Utilisation de la méthode $inc (mongoDB)
+            // Utilisation de la méthode $push  (mongoDB)
+            // Utilisation de la méthode $pull (mongoDB)
+            if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+                console.log("--> L'utilisateur n'est pas dans usersLiked DB et execute une requete like a 1");
+                // Mise à jour DB
+                Sauce.updateOne({ _id: req.params.id },
+                    {
+                        $inc: { likes: 1 },
+                        $push: { usersLiked: req.body.userId }
+                    }
+                )
+                    .then(() => res.status(201).json({ message: "Like + 1" }))
+                    .catch((error) => res.status(400).json({ error }));
+            }
+            // Like = 0 (likes = 0, sans vote)
+
+            // Like = -1 (dislikes = +1)
+
+            // Like = 0 (dislikes = 0, sans vote)
+
+
         })
         .catch((error) => res.status(404).json({ error }));
-    // Like = 1 (likes = +1)
 
-    // Like = 0 (dislikes = 0, sans vote)
-
-    // Like = -1 (dislikes = +1)
-
-    // Like = 0 (dislikes = 0, sans vote)
 };
 
 
