0ee7f640b86b64a244726a6ca59910ff21b7a4c5
MathisPastreNelson <shiray@hotmail.fr>
2022-10-27 14:09:24 +0200

    Modification commentaire

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-// Recherche les différentes erreurs et les gère de manière appropriée
+// Récupère les différentes erreurs et les gère de manière appropriée
 const errorHandler = error => {
     if (error.syscall !== 'listen') {
         throw error;
